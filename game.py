3a4df2efa42067ae27f40cfef2fe7120328a6de0
Arnaud Celermajer <arnaud.celermajer@booking.com>
2019-04-22 18:08:51 +0300

    menu

--- game.py
+++ game.py
@@ -5,20 +5,24 @@ pg.init()
 
 F = pg.display.set_mode((400,700))
 run=1
+mode='menu'
+s_menufnd= pg.image.load('data/space/fndmenu.png');
+s_menu= pg.image.load('data/space/menu.png');
+s_menuc= pg.image.load('data/space/menucredit.png');
+s_menuq= pg.image.load('data/space/menuquit.png');
+s_menus= pg.image.load('data/space/menustart.png');
+def drawmenu():
+    pass
 while run:
-    pg.display.update()
-
-
+    if mode=='menu':
+        drawmenu()
     msgs=pg.event.get()
-
     for m in msgs:
         print(m)
         if m.type == pg.MOUSEMOTION :
             F.fill((m.pos[0] %255,m.pos[1] % 255 ,255))
             #F.fill((255,255,255))
-
         if m.type==pg.QUIT:
-
-
-         run=0
+            run=0
+    pg.display.update()
 print('fin')
